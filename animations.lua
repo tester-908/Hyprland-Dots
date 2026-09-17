0602bb4c02bf6988a848df50a0f6cd576b3639d7
tester-908 <alvar.sahlholm@gmail.com>
2026-09-17 17:39:10 +0200

    changed color

--- animations.lua
+++ animations.lua
@@ -11,7 +11,7 @@ hl.config({
         border_size = 2,
 
         col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
+            active_border   = { colors = {"rgba(990280ee)", "rgba(00ff99ee)"}, angle = 45 },
             inactive_border = "rgba(595959aa)",
         },
 
